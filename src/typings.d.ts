0ccb09702bab2f99c1fa345d74331366bb8b87e7
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2021-09-08 10:40:14 +0300

    fix typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,9 @@
-declare module '*.css' {
-  interface IClassNames {
-    [className: string]: string
-  }
-  const classNames: IClassNames;
-  export = classNames;
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
 }
