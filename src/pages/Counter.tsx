0884a450eaac1b7d0901a682689e4f48900fd6d1
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2021-09-10 09:59:17 +0300

    setup precommit

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Counter: React.FC = () => {
   return (
     <>
-      <h1 className="title">
-        Counter
-        </h1>
+      <h1 className="title">Counter</h1>
     </>
   );
 };
