4fb702df5ff4fbe49b8588483f7f0560c65c1e48
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2021-09-20 16:59:05 +0300

    add complex logic to api service

--- src/pages/Query/services/postsApiService.ts
+++ src/pages/Query/services/postsApiService.ts
@@ -2,6 +2,11 @@ import { IPost } from '../interfaces';
 
 export const getPosts = async (): Promise<IPost[]> => {
   return await fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .catch(error => console.log('An error occurred' + error));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => new Error('An error occurred' + error));
 };
