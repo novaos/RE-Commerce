53862d546c0ba65c57c9a7ff076650b9f83da9da
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2021-09-27 14:35:39 +0300

    interface refactoring

--- src/pages/Users/interfaces/index.ts
+++ src/pages/Users/interfaces/index.ts
@@ -3,20 +3,24 @@ export interface IUser {
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: string;
-    lat: string;
-    lng: string;
-  };
+  address: IUserAddress;
   phone: string;
   website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+  company: IUserCompany;
+}
+
+export interface IUserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: string;
+  lat: string;
+  lng: string;
+}
+
+export interface IUserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
