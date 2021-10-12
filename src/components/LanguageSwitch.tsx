9e2ce025d4f86746d9db26d8e000b0dc1ba16681
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2021-10-12 09:48:20 +0300

    fix

--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -10,11 +10,23 @@ const LanguageSwitch: React.FC<Props> = ({ language, handleLanguageChange }) => 
   return (
     <div className="column is-flex is-align-items-center">
       <label className="radio">
-        <input type="radio" value="en" name="language" onChange={handleLanguageChange} checked={language === 'en'} />
+        <input
+          type="radio"
+          value="en"
+          name="language"
+          onChange={handleLanguageChange}
+          checked={language === LanguageEnum.EN}
+        />
         En
       </label>
       <label className="radio">
-        <input type="radio" value="de" name="language" onChange={handleLanguageChange} checked={language === 'de'} />
+        <input
+          type="radio"
+          value="de"
+          name="language"
+          onChange={handleLanguageChange}
+          checked={language === LanguageEnum.DE}
+        />
         De
       </label>
     </div>
