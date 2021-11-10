89f03238695ac803dd792e264112a8d6d15a7bb4
Lyubomyr Kuzenko <lubko.lintu@gmail.com>
2021-11-10 11:08:49 +0200

    add unit tests

---
+++ src/utils/__tests__/randomString.test.tsx
@@ -0,0 +1,7 @@
+import randomString from '../randomString';
+
+describe('[utils]', () => {
+  it('randomString should generate random string with length 10', () => {
+    expect(randomString(10).length).toBe(10);
+  });
+});
