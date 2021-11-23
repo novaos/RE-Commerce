fe065a27ecfea6e3c145dfe5a2bc695c3d8a8892
Bohdan Omelianets <b.omelianets@levi9.com>
2021-11-23 17:12:11 +0200

    it seems to be working

--- src/pages/MemoryGame/MemoryGame.jsx
+++ src/pages/MemoryGame/MemoryGame.jsx
@@ -5,39 +5,56 @@ const MemoryGame = () => {
     const [size, setSize] = useState(4);
     const [count, setCount] = useState(0);
     const [images, setImages] = useState([]);
+    const [currentImg, setCurrentImg] = useState('');
     const divs = useRef([]);
     const playgroundRef = useRef(null);
 
     useEffect(() => {
+        getImages();
+        //eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        divs.current.forEach(div => div?.classList.remove('passed'));
+        getImages();
+        reset();
+        //eslint-disable-next-line
+    }, [size])
+
+    useEffect(() => {
+        if(count === 2) {
+            setTimeout(reset, 1000)
+        }
+    }, [count])
+
+    function reset() {
+        divs.current.forEach(div => div?.classList.remove('turned'));
+            setCount(0);
+            setCurrentImg('');
+    }
+
+    function getImages() {
         const arr = [];
         for(let i = 1; i < 26; i++) {
             arr.push(import(`../../assets/memoryGame/${i}.jpg`));
         }
         Promise.all(arr)
-        .then(res => {
-            let links = res.map(item => item.default);
-            setImages(suffleArray(links));
-        });
-    }, []);
+            .then(res => res.map(item => item.default))
+            .then(res => setImages((suffleArray(res))));
 
-    useEffect(() => {
-        playgroundRef.current.style.gridTemplateColumns = `repeat(${size}, 100px)`
-    }, [size])
+        playgroundRef.current.style.gridTemplateColumns = `repeat(${size}, 100px)`;
+    }
 
-    useEffect(() => {
-        if(count === 2) {
-            setTimeout(() => {
-                divs.current.forEach(div => div.classList.remove('turned'));
-                setCount(0);
-            }, 1000)
-        }
-    }, [count])
+    function suffleArray(arr) {
+        return [...arr.slice(0, (size * size) / 2), ...arr.slice(0, (size * size) / 2)].sort(() => 0.5 - Math.random());
+    }
 
     function generateBlocks(size) {
         const blocksArr = [];
+
         for(let i = 0; i < size * size; i++) {
             blocksArr.push(
-                <div key={i} ref={el => divs.current[i] = el} className='item' onClick={() => clickHandler(i)}>
+                <div key={i} data-src={images[i]} ref={el => divs.current[i] = el} className='item' onClick={() => clickHandler(i)}>
                     <img className='side back' src={images[i]} alt='' />
                     <div className='side center'>Click the card</div>
                 </div>);
@@ -46,16 +63,19 @@ const MemoryGame = () => {
         return blocksArr;
     }
 
-    function suffleArray(arr) {
-        return [...arr.slice(0, (size * size) / 2), ...arr.slice(0, (size * size) / 2)].sort(() => 0.5 - Math.random());
-    }
-
     function clickHandler(index) {
+        if(count === 0) {
+            setCurrentImg(divs.current[index].dataset.src);
+        }
+
         if(count === 2) return;
 
         if(!divs.current[index].classList.contains('turned')) {
             divs.current[index].classList.add('turned');
-            setCount(count => count + 1);  
+            if(currentImg === divs.current[index].dataset.src) {
+                divs.current.map(div => div.dataset.src === currentImg ? div.classList.add('passed') : div);
+            }   
+            setCount(count => count + 1);
         }
     }
 
@@ -64,10 +84,9 @@ const MemoryGame = () => {
     return (
         <div className='container'>
             <h1 className='title '>Memory game</h1>
-            <select onChange={e => setSize(e.target.value)}>
-                <option value="4">4x4</option>
-                <option value="6">6x6</option>
-                <option value="8">8x8</option>
+            <select value={size} onChange={e => setSize(e.target.value)}>
+                <option value={4}>4x4</option>
+                <option value={6}>6x6</option>
             </select>
             <div className='playground' ref={playgroundRef}>
                 {generateBlocks(size)}
