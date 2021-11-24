209a8c9d5bafb954d7f210d271571998adf361e2
Bohdan Omelianets <b.omelianets@levi9.com>
2021-11-24 12:22:49 +0200

    finish the game

--- src/pages/MemoryGame/MemoryGame.jsx
+++ src/pages/MemoryGame/MemoryGame.jsx
@@ -2,97 +2,120 @@ import React, { useState, useEffect, useRef } from 'react';
 import './memoryGame.scss';
 
 const MemoryGame = () => {
-    const [size, setSize] = useState(4);
-    const [count, setCount] = useState(0);
-    const [images, setImages] = useState([]);
-    const [currentImg, setCurrentImg] = useState('');
-    const divs = useRef([]);
-    const playgroundRef = useRef(null);
+  const [size, setSize] = useState(4);
+  const [wait, setWaiting] = useState(false);
+  const [images, setImages] = useState([]);
+  const [currentImg, setCurrentImg] = useState('');
+  const divs = useRef([]);
+  const modal = useRef(null);
+  const playgroundRef = useRef(null);
 
-    useEffect(() => {
-        getImages();
-        //eslint-disable-next-line
-    }, []);
+  useEffect(() => {
+    getImages(size);
+    //eslint-disable-next-line
+  }, []);
 
-    useEffect(() => {
-        divs.current.forEach(div => div?.classList.remove('passed'));
-        getImages();
-        reset();
-        //eslint-disable-next-line
-    }, [size])
+  function isFinished () {
+    return divs.current.every(div => div?.classList.contains('passed')) && divs.current.length
+  }
 
-    useEffect(() => {
-        if(count === 2) {
-            setTimeout(reset, 1000)
-        }
-    }, [count])
+  useEffect(() => {
+    setImages([])
+    divs.current.forEach(div => div?.classList.remove('passed'));
+    reset();
+    getImages(size);
+    //eslint-disable-next-line
+  }, [size]);
 
-    function reset() {
-        divs.current.forEach(div => div?.classList.remove('turned'));
-            setCount(0);
-            setCurrentImg('');
+  function reset() {
+    divs.current.forEach(div => div?.classList.remove('turned'));
+    setWaiting(false);
+    setCurrentImg('');
+  }
+
+  function getImages(size) {
+    const quantity = (size * size) / 2;
+    const arr = [];
+
+    for (let i = 1; i <= quantity; i++) {
+      arr.push(import(`../../assets/memoryGame/${i}.jpg`));
     }
 
-    function getImages() {
-        const arr = [];
-        for(let i = 1; i < 26; i++) {
-            arr.push(import(`../../assets/memoryGame/${i}.jpg`));
-        }
-        Promise.all(arr)
-            .then(res => res.map(item => item.default))
-            .then(res => setImages((suffleArray(res))));
+    Promise.all(arr)
+      .then(res => res.map(item => item.default))
+      .then(res => setImages(suffleArray(res)));
 
-        playgroundRef.current.style.gridTemplateColumns = `repeat(${size}, 100px)`;
-    }
+    playgroundRef.current.style.gridTemplateColumns = `repeat(${size}, 100px)`;
+  }
 
-    function suffleArray(arr) {
-        return [...arr.slice(0, (size * size) / 2), ...arr.slice(0, (size * size) / 2)].sort(() => 0.5 - Math.random());
-    }
+  function suffleArray(arr) {
+    return [...arr, ...arr].sort(() => 0.5 - Math.random());
+  }
 
-    function generateBlocks(size) {
-        const blocksArr = [];
-
-        for(let i = 0; i < size * size; i++) {
-            blocksArr.push(
-                <div key={i} data-src={images[i]} ref={el => divs.current[i] = el} className='item' onClick={() => clickHandler(i)}>
-                    <img className='side back' src={images[i]} alt='' />
-                    <div className='side center'>Click the card</div>
-                </div>);
-        }
-
-        return blocksArr;
-    }
-
-    function clickHandler(index) {
-        if(count === 0) {
-            setCurrentImg(divs.current[index].dataset.src);
-        }
-
-        if(count === 2) return;
-
-        if(!divs.current[index].classList.contains('turned')) {
-            divs.current[index].classList.add('turned');
-            if(currentImg === divs.current[index].dataset.src) {
-                divs.current.map(div => div.dataset.src === currentImg ? div.classList.add('passed') : div);
-            }   
-            setCount(count => count + 1);
-        }
-    }
-
-
-
-    return (
-        <div className='container'>
-            <h1 className='title '>Memory game</h1>
-            <select value={size} onChange={e => setSize(e.target.value)}>
-                <option value={4}>4x4</option>
-                <option value={6}>6x6</option>
-            </select>
-            <div className='playground' ref={playgroundRef}>
-                {generateBlocks(size)}
-            </div>
+  function generateBlocks(images) {
+    return images.map((img, i) => {
+      return (
+        <div key={i} data-src={img} ref={el => (divs.current[i] = el)} className="item" onClick={() => clickHandler(i)}>
+          <img className="side back" src={img} alt="" />
+          <div className="side center">Click the card</div>
         </div>
-    );
-}
+      );
+    });
+  }
+
+  function clickHandler(index) {
+    if (wait) return;
+
+    if (!currentImg) {
+      setCurrentImg(divs.current[index].dataset.src);
+      divs.current[index].classList.add('turned');
+    } else if (!divs.current[index].classList.contains('turned')) {
+      divs.current[index].classList.add('turned');
+      setWaiting(true);
+
+      if (currentImg === divs.current[index].dataset.src) {
+        divs.current.map(div => (div.dataset.src === currentImg ? div.classList.add('passed') : div));
+      }
+      setTimeout(reset, 900);
+    }
+  }
+
+  function closeModal() {
+    modal.current.classList.remove('is-active')
+  }
+
+  function restartGame() {
+    closeModal();
+    setImages([]);
+    divs.current.forEach(div => div?.classList.remove('passed'));
+    reset();
+    getImages(size);
+  }
+
+  const items = generateBlocks(images);
+
+  return (
+    <>
+      <h1 className="title ">Memory game</h1>
+      <select value={size} onChange={e => setSize(e.target.value)}>
+        <option value={4}>4x4</option>
+        <option value={6}>6x6</option>
+      </select>
+      <div className="playground" ref={playgroundRef}>
+        {items}
+      </div>
+      <div className={`modal ${isFinished() ? 'is-active' : ''}`} ref={modal}>
+        <div className="modal-background"></div>
+        <div className="modal-content">
+        <div className="box">
+          <h1 className='title is-4'>Well done!</h1>
+          <button className='button is-primary' onClick={restartGame}>Start new game</button>
+        </div>
+        </div>
+        <button className="modal-close is-large" aria-label="close" onClick={closeModal}/>
+      </div>
+    </>
+  );
+};
 
 export default MemoryGame;
