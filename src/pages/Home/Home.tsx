2be590fb3f1b1351c4515a8a1d0a2519d1d3f46b
Mykhailo Lysak <m.lysak@ualwo9111nbl.levi9.com>
2021-12-17 11:11:26 +0200

    clear home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 const Home: React.FC = () => {
-  const { t } = useTranslation();
   return (
     <>
-      <h1 className="title">{t('Home.title')}</h1>
-
-      <h5 className="title is-5">Adding SVGs</h5>
-      <Logo />
+      <p>project</p>
     </>
   );
 };
